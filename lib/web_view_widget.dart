253b133a1f9b61375bcef7a132951c319e642a34
ringgavertika <ringgarestu@gmail.com>
2024-07-23 00:12:52 +0700

    news-app-flutter

---
+++ lib/web_view_widget.dart
@@ -0,0 +1,23 @@
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/material.dart';
+
+class ArticleWebView extends StatelessWidget{
+  static const routeName = '/article_web';
+
+  final String url;
+
+  const ArticleWebView({Key? key, required this.url}) : super(key: key);
+
+  @override
+  Widget build (BuildContext context){
+    final controller = WebViewController()..loadRequest(Uri.parse(url));
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('News App'),
+    ),
+    body: WebViewWidget(
+      controller: controller,
+    )
+  );
+  }
+}
